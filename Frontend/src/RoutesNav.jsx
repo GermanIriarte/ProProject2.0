89feeb72256607ce3aba1d203b30c6ef25b302fe
Foodweb <santifernandez0902@gmail.com>
2024-09-12 14:14:57 -0500

    Se arreglaron las rutas del update

--- Frontend/src/RoutesNav.jsx
+++ Frontend/src/RoutesNav.jsx
@@ -37,16 +37,16 @@ function RoutesNav(){
                     <Route path='home/update/:ID_Persona' element= {<UpdatePersona />}></Route>
                     <Route path='/homeProducto' element= {<Producto />}></Route>
                     <Route path='/createProducto' element= {<CreateProducto />}></Route>
-                    <Route path='homeProducto/updateProducto/:Cod_Producto' element= {<UpdateProducto />}></Route>
+                    <Route path='/updateProducto/:Cod_Producto' element= {<UpdateProducto />}></Route>
                     <Route path='/homeProveedor' element= {<Proveedor />}></Route>
                     <Route path='/createProveedor' element= {<CreateProveedor />}></Route>
-                    <Route path='homeProveedor/updateProveedor/:Cod_Proveedor' element= {<UpdateProveedor />}></Route>
+                    <Route path='/updateProveedor/:Cod_Proveedor' element= {<UpdateProveedor />}></Route>
                     <Route path='/Clientes' element= {<Clientes />}></Route>
                     <Route path='/createClientes' element= {<CreateClientes />}></Route>
-                    <Route path='Clientes/updateClientes/:ID_Persona' element= {<UpdateClientes />}></Route>
+                    <Route path='/updateClientes/:ID_Persona' element= {<UpdateClientes />}></Route>
                     <Route path='/Empleados' element= {<Empleados />}></Route>
                     <Route path='/createEmpleados' element= {<CreateEmpleados />}></Route>
-                    <Route path='Empleados/updateEmpleados/:ID_Persona' element= {<UpdateEmpleados />}></Route>
+                    <Route path='/updateEmpleados/:ID_Persona' element= {<UpdateEmpleados />}></Route>
                     
                     
                 </Routes>
